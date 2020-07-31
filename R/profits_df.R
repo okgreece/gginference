7bf547eeb5788704f2ef9d56610972b673962b16
kleanthisk10 <koupidis.okfgr@gmail.com>
2020-07-31 22:54:08 +0300

    update docs, add profits_df dataset

---
+++ R/profits_df.R
@@ -0,0 +1,14 @@
+#' Profits
+#' 
+#' @description 
+#' A data frame with the profits of some companies for 5 months,
+#' constructed for teaching purposes.
+#' 
+#' @usage profits_df
+#' 
+#' @docType data
+#' 
+#' @format A data frame with 26 companies (rows) of 5 months (columns).
+#' @name profits_df
+
+NULL
